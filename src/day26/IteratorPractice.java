0c960cf3f0e7646b656358d8d9b3f2e147850f6a
Taman Neupane <aashish.karn@peacenepal.com>
2022-02-08 21:57:52 +0545

    Collection Practice

---
+++ src/day26/IteratorPractice.java
@@ -0,0 +1,57 @@
+package day26;
+
+import java.util.*;
+import java.util.logging.Logger;
+
+public class IteratorPractice {
+
+    public static void main(String[] args) {
+        Collection<String> stringArrayList = new ArrayList<>();
+        stringArrayList.add("A");
+        stringArrayList.add("C");
+        stringArrayList.add("D");
+        stringArrayList.add("D");
+        stringArrayList.add("B");
+
+        Logger.getGlobal().info(stringArrayList.toString());
+
+        Collection<String> noDuplicateStringList = new TreeSet<>(stringArrayList);
+        Logger.getGlobal().info(noDuplicateStringList.toString());
+
+//        Iterator<String> iterator = stringArrayList.iterator();
+//        while (iterator.hasNext()){
+//            String item = iterator.next();
+//            Logger.getGlobal().info(item);
+//        }
+
+
+//        for (String item :stringArrayList) {
+//            Logger.getGlobal().info(item);
+//        }
+
+//        boolean contains = stringArrayList.contains("A");
+//        Logger.getGlobal().info(String.valueOf(contains));
+//
+//
+//        Collection<Student> students = new LinkedHashSet<>();
+//        students.add(new Student("B", 2));
+//        students.add(new Student("A", 1));
+//        students.add(new Student("C", 1));
+//        students.add(new Student("D", 1));
+//        students.add(new Student("E", 1));
+//
+//        students.add(new Student("A", 1));
+//
+//        Logger.getGlobal().info(students.toString());
+
+//        students.add(new Student("C", 3));
+//        students.add(new Student("D", 4));
+//
+//        boolean sContains = students.contains(new Student("A", 1));
+//        Logger.getGlobal().info(String.valueOf(sContains));
+//
+//        students.clear();
+//        boolean isStudentListEmpty = students.isEmpty();
+//        Logger.getGlobal().info(String.valueOf(isStudentListEmpty));
+    }
+}
---
+++ src/day26/Student.java
@@ -0,0 +1,40 @@
+package day26;
+
+import java.util.Objects;
+
+public class Student implements Comparable<Student> {
+
+    private String name;
+    private int rollNumber;
+
+    public Student(String name, int rollNumber) {
+        this.name = name;
+        this.rollNumber = rollNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return rollNumber == student.rollNumber && Objects.equals(name, student.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, rollNumber);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", rollNumber=" + rollNumber +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        return this.name.compareTo(o.name);
+    }
+}
